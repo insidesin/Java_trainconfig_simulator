6a6fdf26c12116b699565683f326a857682f4506
Yeo Fei Wen <feiwen.yeo@connect.qut.edu.au>
2013-05-20 18:35:03 +1000

    Finish Test for removeCarriage and trainCanMove
    Includes test list (Updated) in .pdf

---
+++ asgn2Full/asgn02/src/asgn2RollingStock/Locomotive.java
@@ -0,0 +1,103 @@
+package asgn2RollingStock;
+
+import asgn2Exceptions.TrainException;
+
+/**
+ * 
+ * @author Jackson Powell, Yeo Fei Wen
+ * 
+ **/
+public class Locomotive extends RollingStock {
+
+	private int powerValue;
+	private String locomotiveClassification;
+	private String engineClass;
+
+	private Integer enginePower = 100;
+
+	private final String classificationError = "Starting with a power value of (1-9), followed by"
+			+ "an engine type of either 'E' for Electrical, 'D' for Diesel or 'S' for Steam. "
+			+ "An example will be '4S'.";
+
+	/**
+	 * Constructs a Locomotive RollingStock with the supplied gross weight
+	 * and power classification. Must comply with specified restrictions and
+	 * regulations in power to weight ratios.
+	 * 
+	 * @param grossWeight
+	 *            The gross weight associated with this type of locomotive.
+	 * @param classification
+	 *            The type of engine this locomotive have.
+	 * @throws TrainException
+	 *             If the gross weight is not a positive Integer or the power
+	 *             classification is invalid.
+	 */
+	public Locomotive(Integer grossWeight, String classification)
+			throws TrainException {
+		super(grossWeight);
+
+		if (classification == null) {
+			throw new TrainException(
+					"A locomotive classification have to be added with these guidelines:"
+							+ classificationError);
+		}
+
+		if (classification.length() != 2) {
+			throw new TrainException(
+					"A locomotive classification have to consist of only two characters. "
+							+ classificationError);
+		}
+
+		int powerValue = Integer.parseInt(classification.substring(0, 1));
+		String engineClass = classification.substring(1, 2);
+
+		if (powerValue < 1 || powerValue > 9) {
+			throw new TrainException(
+					"Invalid power class in classification. Must define using the guidelines. "
+							+ classificationError);
+		}
+
+		if (!engineClass.equals("S") && !engineClass.equals("D")
+				&& !engineClass.equals("E")) {
+			throw new TrainException("Invalid engine type in classification."
+					+ engineClass + " Must define using the guidelines. "
+					+ classificationError);
+		}
+
+		/*if (((powerValue * enginePower) - grossWeight) <= 0) {
+			throw new TrainException(
+					"Locomotive cannot generate the amount of power to pull and move. "
+							+ "Please use a higher power class higher than : "
+							+ powerValue);
+		}*/
+
+		this.locomotiveClassification = classification;
+
+		// PowerClass (0-9)
+		this.powerValue = powerValue;
+
+		// Engine Type (Steam / Diesel / Electric)
+		this.engineClass = engineClass;
+	}
+
+	/**
+	 * Returns how much total weight the locomotive can pull (including itself)
+	 * as classified by locomotive power specifications.
+	 * 
+	 * @return The locomotive's "pulling power" in tonnes.
+	 */
+	public Integer power() {
+		return ((powerValue * enginePower) - this.getGrossWeight());
+	}
+
+	@Override
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see asgn2RollingStock.RollingStock#toString()
+	 */
+	public String toString() {
+		return "Loco(" + locomotiveClassification + ")";
+	}
+
+}
---
+++ asgn2Full/asgn02/src/asgn2RollingStock/PassengerCar.java
@@ -0,0 +1,128 @@
+package asgn2RollingStock;
+
+import asgn2Exceptions.TrainException;
+
+/**
+ * 
+ * @author Jackson Powell, Yeo Fei Wen
+ * 
+ */
+public class PassengerCar extends RollingStock {
+
+	private Integer numOfSeats;
+	private Integer onBoardPassengers;
+
+	/**
+	 * Constructs a passenger car with a known weight and a fixed number of
+	 * seats. (We allow a passenger car to have zero seats, although it would
+	 * not be very useful.)
+	 * 
+	 * @param grossWeight
+	 *            The carriage's gross weight in tonnes (ignoring the weight of
+	 *            passengers, which we treat as negligible)
+	 * @param numberOfSeats
+	 *            The number of seats to be used in the carriage
+	 * @throws TrainException
+	 *             If the gross weight is not a positive Integer or if the number of seats is
+	 *             negative
+	 */
+	public PassengerCar(Integer grossWeight, Integer numberOfSeats)
+			throws TrainException {
+		super(grossWeight);
+
+		if (numberOfSeats == null) {
+			throw new TrainException(
+					"Number of seats must be entered and must be a valid Integer.");
+		}
+
+		if (numberOfSeats < 0) {
+			throw new TrainException(
+					"Number of seats cannot be a negative. Only 0 or more seats can be set.");
+		}
+
+		this.numOfSeats = numberOfSeats;
+		this.onBoardPassengers = 0;
+	}
+
+	/**
+	 * Adds the given number of new passengers to the number on board the
+	 * carriage. If there are too many new passengers for the number of spare
+	 * seats the left over people are not boarded.
+	 * 
+	 * @param newPassengers
+	 *            The number of people to board onto the carriage.
+	 * @return The number of people who were unable to board the carriage
+	 *         because they couldn't get a seat.
+	 * @throws TrainException
+	 *            If the number of new passengers is negative.
+	 */
+	public Integer board(Integer newPassengers) throws TrainException {
+		if (newPassengers < 0)
+			throw new TrainException(
+					"Number of passengers cannot be a negative. Only 0 or more can board.");
+		
+		Integer unableToBoardPassengers = 0;
+
+		if((onBoardPassengers + newPassengers) <= numOfSeats) {
+			this.onBoardPassengers += newPassengers;
+		} else {
+			unableToBoardPassengers = ((onBoardPassengers + newPassengers) % numOfSeats);
+			this.onBoardPassengers = numOfSeats;
+		}
+		return unableToBoardPassengers;
+	}
+
+	/**
+	 * Removes the given number of passengers from this carriage. Attempting to
+	 * remove more passengers than are on board will throw an exception.
+	 * 
+	 * @param departingPassengers
+	 *            The number of passengers alighting from the carriage.
+	 * @throws TrainException
+	 *             If the number of departing passengers is negative or if the
+	 *             number of departing passengers exceeds the number on board.
+	 */
+	public void alight(Integer departingPassengers) throws TrainException {
+		if (departingPassengers < 0) {
+			throw new TrainException(
+					"The number of departing passengers cannot be less than 0.");
+		}
+
+		if (departingPassengers > onBoardPassengers) {
+			throw new TrainException(
+					"The number of departing passengers have to be less or equal to the"
+							+ "number of people on board");
+		}
+
+		onBoardPassengers -= departingPassengers;
+	}
+
+	/**
+	 * Returns the number of passengers currently on board this carriage.
+	 * 
+	 * @return The number of passengers on board.
+	 */
+	public Integer numberOnBoard() {
+		return onBoardPassengers;
+	}
+
+	/**
+	 * Returns the number of seats installed on this carriage.
+	 * 
+	 * @return The number of seats on this carriage.
+	 */
+	public Integer numberOfSeats() {
+		return numOfSeats;
+	}
+
+	@Override
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see asgn2RollingStock.RollingStock#toString()
+	 */
+	public String toString() {
+		return "Passenger(" + onBoardPassengers + "/" + numOfSeats + ")";
+	}
+
+}
